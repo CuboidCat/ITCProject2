c25f5fdc0792d35118eef04d350bae1e86797d6a
Hunter <hunterliamwallace@gmail.com>
2024-10-23 11:48:02 -0500

    Reinstalled Tailwind due to some problems
    Continued work on navBar. Added a large screen design and made transitions to make it more fluid. I also needed to reinstall tailwind.css

--- src/Navbar/navBar.js
+++ src/Navbar/navBar.js
@@ -1,6 +1,19 @@
 function toggleMenu() {
     const links = document.getElementById("mobileLinks");
     links.classList.toggle('hidden');
+
+    setTimeout(function(){
+        for (const child of links.children) {
+            if (!links.classList.contains('hidden')) {
+                child.classList.replace("opacity-0", "opacity-100");
+                console.log("Test");
+            }
+            else {
+                child.classList.replace("opacity-100", "opacity-0")
+            }
+        }
+    }, 0);
+
 }
 
 
